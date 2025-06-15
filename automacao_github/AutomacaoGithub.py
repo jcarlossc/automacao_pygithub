5ea1600155f1919567a5aa83006f1f6e6457aae7
jcarlossc <josecscosta@fac.pe.senac.br>
2025-06-15 16:34:51 -0300

    Método criar epositório adicionado.

--- automacao_github/AutomacaoGithub.py
+++ automacao_github/AutomacaoGithub.py
@@ -2,14 +2,20 @@ from github import Github
 
 class AutomacaoGithub:
     
-    def __init__(self, TOKEN):
+    def __init__(self, TOKEN) -> None:
         self.token = TOKEN
         self.github_objeto = Github(TOKEN)
 
-    def listar_repositorio(self):    
-        lista = self.github_objeto.get_user()
-        for i, repositorio in enumerate(lista.get_repos(), start = 1):
+    def listar_repositorios(self) -> None:    
+        usuario = self.github_objeto.get_user()
+        for i, repositorio in enumerate(usuario.get_repos(), start = 1):
             print(f"{i}. {repositorio.name} | {'Privado' if repositorio.private else 'Público'} | {repositorio.html_url}")
 
-
-
+    def criar_repositorio(self, nome: str, descricao: str) -> None:
+        usuario = self.github_objeto.get_user()
+        novo_repositorio = usuario.create_repo(
+            name = nome,
+            description = descricao,
+            private = False
+        )
+        print(f"Repositório criado: {novo_repositorio.html_url}")
